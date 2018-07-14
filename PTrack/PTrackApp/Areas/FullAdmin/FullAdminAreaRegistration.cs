34985f6469bab1449226ec841bd5138e4c6d2ebc
Kelvin <khoa.pham2502@gmail.com>
2018-07-14 15:39:33 +0700

    Alpha Update Begin

---
+++ PTrack/PTrackApp/Areas/FullAdmin/FullAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PTrackApp.Areas.FullAdmin
+{
+    public class FullAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "FullAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FullAdmin_default",
+                "FullAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ PTrack/PTrackApp/Areas/UserTrack/UserTrackAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PTrackApp.Areas.UserTrack
+{
+    public class UserTrackAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UserTrack";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UserTrack_default",
+                "UserTrack/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

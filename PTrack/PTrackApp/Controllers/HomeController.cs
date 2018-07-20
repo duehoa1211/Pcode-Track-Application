1a16e40686e5346295a6ca22bf886ca141a03ecd
Kelvin <khoa.pham2502@gmail.com>
2018-07-21 02:03:11 +0700

    Update Alpha 20-7-2018

--- PTrack/PTrackApp/Controllers/HomeController.cs
+++ PTrack/PTrackApp/Controllers/HomeController.cs
@@ -4,26 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Dapper;
+using System.Configuration;
+
 namespace PTrackApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IDbConnection _data;
+        public HomeController()
+        {
+            _data = new SqlConnection(ConfigurationManager.ConnectionStrings["PCodeTrackDataBase"].ConnectionString);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = "PCODE TRACK";
             return View();
         }
     }
